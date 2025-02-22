25cf33de62505c3a56a40f0d1d8109ce8529e717
kanhaiya=bhayana <kanhaiyabhayana1@gmail.com>
2025-02-22 17:21:07 +0530

    Decorator - fixed

--- Decorator/Main.java
+++ Decorator/Main.java
@@ -1,11 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-
-        Coffee coffee = new EspressoCoffee();
+        
+        Coffee coffee = new SimpleCoffee();
         System.out.println(coffee.getDescription() + " $ " + coffee.getCost());
 
-        // coffee = new MilkDecorator(coffee);
-        // System.out.println(coffee.getDescription() + " $ " + coffee.getCost());
+        coffee = new MilkDecorator(coffee);
+        System.out.println(coffee.getDescription() + " $ " + coffee.getCost());
 
         coffee = new SugarDecorator(coffee);
         System.out.println(coffee.getDescription() + " $ " + coffee.getCost());
